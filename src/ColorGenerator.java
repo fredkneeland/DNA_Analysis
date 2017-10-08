19c0f5aad2bb0636656259fdbbc8d66ac1676f06
Y. S <ThePhantomGamer@users.noreply.github.com>
2017-10-07 20:55:43 -0400

    Create ColorGenerator.java

---
+++ src/ColorGenerator.java
@@ -0,0 +1,14 @@
+class ColorGenerator {
+  // [0] => Red Color from 0-255
+  // [1] => Green Color from 0-255
+  // [2] => Blue Color from 0-255
+  public static int[] colorGeneratorForLetter(Char letter) {
+     switch(letter) {
+         // TODO: return a const color here for each letter
+      }
+  } 
+
+  public static int[] averageColorForLetterCounts(int aCount, int tCount, int gCount, int cCount) {
+     // use colorGeneratorForLetter and averages to determine color
+   }
+}
